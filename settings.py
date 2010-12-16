7263bb5819be665b2bfdddd931feecc3c8fe75f4
Clint Savage <herlo1@gmail.com>
2010-12-15 20:52:01 -0700

    initial settings.py

---
+++ settings.py
@@ -0,0 +1,15 @@
+# PyCamps configurations loader
+
+import os
+
+# the path to the git repo to clone 
+
+GIT_ROOT = '/home/clints/Projects/GitPyCamps/'
+
+# the dir where the camp will be stored within the 
+# user's home directory camp will be turned into 
+# /home/user/camp4 for the 4th camp.
+
+CAMP_NAME = 'camp'
+
+
