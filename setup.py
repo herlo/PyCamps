3b8c94e32ef8d98135f5aeaf01bc34b3e881cdbb
Clint Savage <herlo1@gmail.com>
2011-04-15 19:46:23 -0600

    trying out a few things

--- setup.py
+++ setup.py
@@ -5,17 +5,20 @@ import os
 from distutils.core import setup
 from distutils.sysconfig import get_python_lib
 
+import src.pycamps.__version__
+import src.pycamps.__prog__
+
 pkgs_path = get_python_lib()
 app_name = 'pycamps'
 
-setup(name=app_name,
-    version='0.1',
+setup(name=__prog__,
+    version=__version__,
     description='Python Developer Camps',
     author='Clint Savage',
     author_email='herlo1@gmail.com',
     url='https://github.com/herlo/PyCamps',
-    packages=['pycamps',],
+    packages=['pycamps', 'pycamps.config', 'pycamps.contrib', 'pycamps.contrib.hooks'],
     package_dir={'pycamps': 'src/pycamps'},
-    scripts=['pycamps',],
-    package_data={'pycamps': ['settings.py.sample']},
+    scripts=['pc',],
+    package_data={'pycamps.config': ['settings.py.sample']},
 )
